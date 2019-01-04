55a02b96fbd12430bf5c47d54b6f4c72fd9290ed
Jordan <jordan.carlile@gmail.com>
2019-01-03 19:00:01 -0800

    added some game logic features: guessing, updating the underscores in html view

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,78 @@
-console.log("Hello World");
+// Global Variables
+const words = ["pikachu", "squirtle", "charmander", "bulbasaur"];
+let chosenWord = "";
+let wrongGuesses = [];
+let numGuesses = 0;
+let candidate = [];
+// Function Definitions
+
+function startGame() {
+    
+    // initialize to 9 guesses
+    numGuesses = 9;
+
+    chosenWord = words[Math.floor(Math.random() * words.length)]
+    console.log("Chosen word is:", chosenWord);
+
+    let numBlanks = chosenWord.length;
+
+    // initize underscores
+    for(let i = 0; i < numBlanks; i++){
+        candidate.push("_");
+    }
+
+
+
+}
+
+
+function checkLetter(letter) {
+
+    let correct = false;
+
+    for(let i = 0; i < chosenWord.length; i++) {
+        if (chosenWord[i] == letter){
+            correct = true;
+        }
+    }
+
+    if (correct) {
+        // guessed correctly
+
+        for(let j = 0; j < chosenWord.length; j++) {
+            if (chosenWord[j] == letter) {
+                candidate[j] = letter
+            }
+        }
+    }
+    else {
+        // guessed wrong
+
+        wrongGuesses.push(letter);
+        numGuesses--;
+        
+
+    }
+
+}
+
+function roundComplete() {
+    // update HMTL
+    document.getElementById("guesses-left").innerHTML = numGuesses;
+
+    document.getElementById("word-blanks").innerHTML = candidate.join(" ");
+
+    
+
+}
+
+// Main Process
+startGame();
+
+document.onkeyup = function(event) {
+    let letterGuessed = event.key;
+
+    checkLetter(letterGuessed);
+
+    roundComplete();
+}
